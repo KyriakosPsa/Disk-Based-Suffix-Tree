c19d3d2116f005acf1af07eda239a3fef7f0c69e
Kyriakos Psallidas <kyriakos.psallidas@gmail.com>
2023-04-26 17:37:29 +0300

    Added error catching

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <regex>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "header.h"
 
 using namespace std;
 
 int main()
 {
-  // Calculate t
-  int seq_size = 1000000;
-  float t = calculate_t(seq_size);
-  cout << (int)t;
+  // Calculate t test
+  int seq_size = 0;
+  int limit_memory_to = 0;
+  // Pass sequence lenght
+  cout << "Enter your sequence lenght:\n";
+  cin >> seq_size;
+  // pass available RAM
+  cout << "Enter the RAM memory you wish to use:\n";
+  cin >> limit_memory_to;
+  try
+  {
+    int t = calculate_t(seq_size, true, limit_memory_to);
+    cout << "The value of t is:\n"
+         << t;
+  }
+  catch (std::exception &e)
+  { // catch the exception
+    cerr << "Error: " << e.what();
+  }
+  // calculate partitions test
 }
