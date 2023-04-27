c3a8aca73ea18603463f1e5e27bb059797e86a7b
Kyriakos Psallidas <kyriakos.psallidas@gmail.com>
2023-04-27 18:51:55 +0300

    Changed variable types

--- t_calculator.cpp
+++ t_calculator.cpp
@@ -1,8 +1,4 @@
-#include <regex>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <set>
 #include <cmath>
 // Relevant system libaries for windows and linux systems
 // If the system is windows, include the windows.h library
@@ -16,7 +12,7 @@
 using namespace std;
 
 // Use either the windows or linux specific code to get the total ram
-float get_total_ram_bytes()
+long long int get_total_ram_bytes()
 /*This function returns the total memory of a windows or linxus system in bytes,
 it is utilized to make that value available for the calculation of t*/
 {
@@ -25,40 +21,41 @@ it is utilized to make that value available for the calculation of t*/
   MEMORYSTATUSEX memory_status;
   memory_status.dwLength = sizeof(memory_status);
   GlobalMemoryStatusEx(&memory_status);
-  return (float)memory_status.ullTotalPhys;
+  return memory_status.ullTotalPhys;
 #else
   // Linux-specific code
   struct sysinfo system_info;
   sysinfo(&system_info);
-  return (float)(system_info.totalram * system_info.mem_unit);
+  return (system_info.totalram * system_info.mem_unit);
 #endif
 }
 
-float calculate_t(int &sequence_size, bool limit = false, int limit_memory_to = 2)
+long long int calculate_t(long long int &sequence_size, bool limit = false, int limit_memory_to = 0)
 /*This function is utilized to calculate the t-threshold parameter
 based on the equation t <= M - n/4)/72, where:
 M -> memory in bytes
 n/4 -> input string compressed to 1/4 bytes per characted
 72 -> bytes per tree for a single suffix link*/
 {
-  float total_ram = get_total_ram_bytes();
+  long long int total_ram = get_total_ram_bytes();
   if (limit == false)
   {
-    float t = (total_ram - sequence_size / 4) / 72;
-    return (int)t;
+    unsigned long int t = (total_ram - sequence_size / 4) / 72;
+    return t;
   }
   else
   {
     // convert GB to bytes
-    float limited_ram = limit_memory_to * pow(2, 30);
+    long long int limited_ram = limit_memory_to * pow(10, 9);
+    // check if the passed limited ram is higher than the available ram
     if (limited_ram > total_ram)
     {
       throw invalid_argument("Limited RAM cannot exceed your system available RAM");
     }
     else
     {
-      float t = (limited_ram - sequence_size / 4) / 72;
-      return (int)t;
+      long long int t = (limited_ram - (sequence_size / 4)) / 72;
+      return t;
     }
   }
 }
