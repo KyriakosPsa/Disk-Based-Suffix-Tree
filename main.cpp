f8d6f0dffcac878f3c8998693a3d4bbbbe8e6f8a
Kyriakos Psallidas <kyriakos.psallidas@gmail.com>
2023-04-27 18:53:21 +0300

    User input to limite memory

--- main.cpp
+++ main.cpp
@@ -8,24 +8,35 @@ using namespace std;
 
 int main()
 {
-  // Calculate t test
-  int seq_size = 0;
-  int limit_memory_to = 0;
-  // Pass sequence lenght
-  cout << "Enter your sequence lenght:\n";
-  cin >> seq_size;
-  // pass available RAM
-  cout << "Enter the RAM memory you wish to use:\n";
-  cin >> limit_memory_to;
+  // Initialize variables for the calculation of t
+  long long int seq_size = 3200000000;
+  int limit_memory_to;
+  char answer;
+  bool limit_memory;
+
+  // Ask the user if they want to limit the memory of the Algorithm in GB
+  cout << "Do you wish to limit the available memory of the algorithm? (y/n):";
+  cin >> answer;
+  if (answer == 'y')
+  {
+    limit_memory = true;
+    cout << "Enter the RAM memory you wish to use:\n";
+    cin >> limit_memory_to;
+  }
+  else
+  {
+    limit_memory = false;
+  }
+
+  // Calculate t
   try
   {
-    int t = calculate_t(seq_size, true, limit_memory_to);
+    long long int t = calculate_t(seq_size, limit_memory, limit_memory_to);
     cout << "The value of t is:\n"
          << t;
   }
   catch (std::exception &e)
-  { // catch the exception
+  { // catch the exception if limited memory > RAM
     cerr << "Error: " << e.what();
   }
-  // calculate partitions test
 }
