1648cafaf14cdc65715ca239707ce73c26e88ee6
Kyriakos Psallidas <kyriakos.psallidas@gmail.com>
2023-04-29 13:49:17 +0300

    Fixed memory availability issue

--- main.cpp
+++ main.cpp
@@ -2,41 +2,60 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 #include "header.h"
 
 using namespace std;
 
 int main()
 {
-  // Initialize variables for the calculation of t
+  // find the sequence_size from the file
+  // string filename;
+  // cout << "Enter the DNA sequence filename: \n";
+  // cin >> filename;
   long long int seq_size = 3200000000;
-  int limit_memory_to;
+
+  // Initialize variables for the calculation of t
+  long long int limited_ram_bytes;
+  int limited_memory_GB;
   char answer;
-  bool limit_memory;
+  bool limit_memory_bool;
 
   // Ask the user if they want to limit the memory of the Algorithm in GB
-  cout << "Do you wish to limit the available memory of the algorithm? (y/n):";
+  cout << "Do you wish to limit the available memory for the algorithm? (y/n): \n";
   cin >> answer;
   if (answer == 'y')
   {
-    limit_memory = true;
+    limit_memory_bool = true;
     cout << "Enter the RAM memory you wish to use:\n";
-    cin >> limit_memory_to;
+    cin >> limited_memory_GB;
+    limited_ram_bytes = limited_memory_GB * pow(10, 9);
   }
   else
   {
-    limit_memory = false;
+    limit_memory_bool = false;
   }
-
-  // Calculate t
+  // Get the total available memory
+  long long int available_total_RAM = get_total_ram_bytes();
+  // Calculate t, throw error if limit_memory > available RAM
   try
   {
-    long long int t = calculate_t(seq_size, limit_memory, limit_memory_to);
-    cout << "The value of t is:\n"
-         << t;
+    if (limited_ram_bytes < available_total_RAM)
+    {
+      long long int t = calculate_t(seq_size,
+                                    available_total_RAM,
+                                    limit_memory_bool,
+                                    limited_ram_bytes);
+      cout << "The value of t is:\n"
+           << t;
+    }
+    else
+    {
+      throw invalid_argument("Limited memory cannot be higher than available system RAM");
+    }
   }
-  catch (std::exception &e)
-  { // catch the exception if limited memory > RAM
-    cerr << "Error: " << e.what();
+  catch (invalid_argument &e)
+  {
+    cerr << e.what() << "\n";
   }
 }
