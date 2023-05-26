94159ff4fd56329051852cad7905d3f965579027
Vasilis <vasdimako@gmail.com>
2023-05-26 14:59:35 +0300

    query prefix, split tree, visualize no leaves

--- suffix_tree.cpp
+++ suffix_tree.cpp
@@ -24,25 +24,47 @@ public:
         // The child nodes of the node are inserted into the end of the children vector.
         m_children.insert(m_children.end(), children);
     }
+
+    Node(const string &sub, vector<int> &children, int parent) : m_sub(sub), m_parent(parent)
+    {
+        // The child nodes of the node are inserted into the end of the children vector.
+        m_children.insert(m_children.end(), children.begin(), children.end());
+    }
+
+    size_t getSubLength() {
+        return m_sub.length();
+    }
 };
 
 class SuffixTree
 {
 public:
-    std::unordered_map<int, int> m_leaves; // Key -> Node unique id , Value -> suffix index in the original string
     std::unordered_map<int, Node> m_nodes;
+    std::unordered_map<int, int> m_leaves; // Key -> Node unique id , Value -> suffix index in the original string
     size_t m_length{0};
+    int m_rootId{0};
+
+    SuffixTree() {
+
+    }
+
+    SuffixTree(std::vector<std::pair<int, Node>> nodes, size_t length, int rootId) {
+        m_length = length;
+        m_rootId = rootId;
+        for (auto node: nodes) {
+            m_nodes.insert(node);
+        }
+    }
 
     // Construct the suffix tree from a specified string
     void build(const string &str)
     {
         m_length = str.length();
         // Initialize the tree with an empty root node.
-        if (m_nodes.count(0) == 0)
-        {
-            m_nodes.emplace(m_id, Node{});
-            m_id++;
-        }
+        // root parent = -1
+        m_nodes.emplace(m_id, Node{"", {}, -1});
+        m_rootId = m_id;
+        m_id++;
         // Iterate over each character in the input string.
         for (size_t i = 0; i < str.length(); i++)
         {
@@ -144,7 +166,7 @@ public:
                 int parentNode{0};
                 parentlen += element.second.m_sub.length(); // Add the length of the leaf node substring
                 parentNode = element.second.m_parent;       // Pass the first parent
-                while (m_nodes.count(parentNode) > 0)       // Iterate over all available parents
+                while (parentNode >= 0)       // Iterate over all available parents
                 {
                     parentlen += m_nodes.at(parentNode).m_sub.length(); // Extended the length of the path based on each parent substring
                     parentNode = m_nodes.at(parentNode).m_parent;
@@ -190,6 +212,127 @@ public:
         };
 
         f(0, "");
+    }
+
+    void visualizeNoLeaves()
+    {
+        if (m_nodes.size() == 0)
+        {
+            cout << "<empty>\n";
+            return;
+        }
+
+        function<void(int, const string &)> f;
+        f = [&](int id, const string &pre)
+        {
+            auto children = m_nodes.at(id).m_children;
+            if (children.size() == 0)
+            {
+                cout << "- " << m_nodes.at(id).m_sub << '\n';
+                return;
+            }
+            cout << "+ " << m_nodes.at(id).m_sub << '\n';
+
+            auto it = begin(children);
+            if (it != end(children))
+                do
+                {
+                    if (next(it) == end(children))
+                        break;
+                    cout << pre << "+-";
+                    f(*it, pre + "| ");
+                    it = next(it);
+                } while (true);
+
+            cout << pre << "+-";
+            f(children[children.size() - 1], pre + "  ");
+        };
+
+        f(m_rootId, "");
+    }
+
+    void getAllChildren(int node, std::vector<std::pair<int, Node>> &children) {
+        for (auto child: m_nodes.at(node).m_children) {
+            children.push_back(std::make_pair(child, m_nodes.at(child)));
+            this->getAllChildren(child, children);
+        }
+    }
+
+    vector<int> queryPrefix(const string &str) {
+        int currentNode = 0;
+        int flag = currentNode;
+        std::string remaining = str;
+        vector<int> involvedNodes;
+        while (true)
+        {
+            for (auto child : m_nodes.at(currentNode).m_children)
+            {
+                std::string nodeStr = m_nodes.at(child).m_sub;
+                size_t searchResult;
+                if (nodeStr.length() > remaining.length())
+                {
+                    searchResult = nodeStr.rfind(remaining, 0);
+                    if (searchResult == 0)
+                    {
+                        remaining = "PARTIAL";
+                    }
+                }
+                else
+                {
+                    searchResult = remaining.rfind(nodeStr, 0);
+                    if (searchResult == 0)
+                    {
+                        remaining = remaining.substr(nodeStr.length());
+                    }
+                }
+
+                if (searchResult == 0)
+                {
+                    if (remaining.length() == 0)
+                    {
+                        involvedNodes.push_back(child);
+                        return involvedNodes;
+                    } else if (remaining == "PARTIAL") {
+                        involvedNodes.push_back(-child);
+                        return involvedNodes;
+                    }
+                    else
+                    {
+                        involvedNodes.push_back(child);
+                        currentNode = child;
+                        break;
+                    }
+                }
+            }
+            if (flag == currentNode)
+            {
+                std::cout << "Not found in tree: " << remaining << '\n';
+                return involvedNodes;
+            }
+            else
+            {
+                flag = currentNode;
+            }
+        }
+    }
+    void deleteNode(int node) {
+        m_nodes.erase(node);
+    }
+
+    void removeAsChild(int node) {
+        int parent = m_nodes.at(node).m_parent;
+        if (m_nodes.contains(parent)) {
+            vector<int>* children = &m_nodes.at(parent).m_children;
+            std::vector<int>::iterator position = std::find((*children).begin(), (*children).end(), node);
+            if (position != (*children).end()) // == myVector.end() means the element was not found
+                (*children).erase(position);
+        }
+    }
+
+    void deleteNode(std::vector<std::pair<int, Node>> nodes) {
+        for (auto node: nodes) {
+            deleteNode(node.first);
+        }
     }
 
 private:
@@ -271,6 +414,26 @@ private:
     }
 };
 
+SuffixTree* splitTree(SuffixTree &tree, const std::string &str, int prefixLocation) {
+    std::string rootString;
+    if (prefixLocation < 0) {
+        prefixLocation = -prefixLocation;
+        rootString = tree.m_nodes.at(prefixLocation).m_sub;
+    } else {
+        rootString = str;
+    }
+    std::vector<std::pair<int, Node>> newNodes;
+
+    // add new root node and remove it as child from parent in old tree
+    Node newRoot{rootString, tree.m_nodes.at(prefixLocation).m_children, -1};
+    newNodes.push_back(std::make_pair(prefixLocation, newRoot));
+    tree.removeAsChild(prefixLocation);
+
+    tree.getAllChildren(prefixLocation, newNodes);
+    tree.deleteNode(newNodes);
+    return new SuffixTree(newNodes, str.length(), prefixLocation);
+}
+
 int main()
 {
     SuffixTree tree{};
@@ -278,5 +441,15 @@ int main()
     std::cout << std::boolalpha;
     std::cout << tree.isUnique("BAN") << '\n';
     tree.makeLeaves();
+    tree.visualize();
+    cout << '\n';
+
+    std::string prefix{"ANA"};
+    vector<int> location = tree.queryPrefix(prefix);
+    if (!(location.empty())) {
+        SuffixTree newTree = *(splitTree(tree, prefix, location.back()));
+        newTree.visualizeNoLeaves();
+        cout << '\n';
+    }
     tree.visualize();
 }
